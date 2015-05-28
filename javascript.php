bb33cfb3e51d8372685024bcc17dd556df3a6f60
LOCOSP <lokacyjny@gmail.com>
2015-05-28 02:00:11 +0200

    z pliku javascript.php usunąłem:
        <script>
        $(document).ready(function() {
    		    $("#myCarousel").swiperight(function() {
    		    	$("#myCarousel").carousel('prev');
    		    });
    		    $("#myCarousel").swipeleft(function() {
    		    	$("#myCarousel").carousel('next');
    		    });
    			
        });
            
        </script>
    
    nie wykorzystany bajer :)

--- javascript.php
+++ javascript.php
@@ -1,16 +1,3 @@
 <script src="http://code.jquery.com/jquery-latest.js"></script>
 <script src="../framework/bootstrap/2.0/js/bootstrap.min.js"></script>
 <script src="../framework/jquery/jquery.mobile.custom.min.js"></script>
-
-    <script>
-    $(document).ready(function() {
-		    $("#myCarousel").swiperight(function() {
-		    	$("#myCarousel").carousel('prev');
-		    });
-		    $("#myCarousel").swipeleft(function() {
-		    	$("#myCarousel").carousel('next');
-		    });
-			
-    });
-        
-    </script>
